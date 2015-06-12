a4e7753ea3bcc0dd203e4b9e2c2364f7256cd416
JuHyun Kwon <lanoir@gmail.com>
2015-06-13 00:32:54 +0900

    add tags to train

--- models/train.js
+++ models/train.js
@@ -4,6 +4,8 @@ function Train( _id, _name, _description ) {
 	this.id = _id;
 	this.name = _name;
 	this.description = _description;
+	
+	this.tags = [];
 }
 
 Train.prototype.getId = function() {
@@ -17,4 +19,34 @@ Train.prototype.getName = function() {
 Train.prototype.getDescription = function() {
 	return this.description;
 }
+
+Train.prototype.getCreator = function() {
+	return this.creator;
+}
+
+Train.prototype.addTag = function( _tag ) {
+	if( this.hasTag( _tag ) == true ) {
+		return;	
+	}
+	
+	this.tags.push( _tag );	
+}
+
+Train.prototype.removeTag = function( _tag ) {
+	if( this.hasTag( _tag ) == false ) {
+		return;	
+	}
+	
+	this.tags = this.tags.filter( function(e) { return e != _tag; } );
+}
+
+Train.prototype.hasTag = function( _tag ) {
+	return this.tags.indexOf( _tag ) >= 0;
+}
+
+Train.prototype.getTags = function() {
+	return this.tags;
+}
+
+
 module.exports = Train;
--- test/test_train.js
+++ test/test_train.js
@@ -4,11 +4,26 @@ var Train = require('../models/train.js')
 module.exports = {
 	
 	testBasic: function(test) {
-		var train = new Train( 0, 'add sub task feature', 'Split a task into several sub tasks' );
+		var train = new Train( 0, 'add sub task feature', 'Split a task into several sub tasks', null );
 		
 		test.equal( train.getId(), 0 );
 		test.equal( train.getName(), 'add sub task feature' );
 		test.equal( train.getDescription(), 'Split a task into several sub tasks' );
 		test.done();
+	},
+
+	testTags: function(test) {
+		var train = new Train( 0, 'add sub task feature', 'Split a task into several sub tasks', null );
+		
+		train.addTag( 'cloud' );
+		
+		test.ok( train.hasTag('cloud') );
+		
+		train.removeTag( 'cloud' );
+		
+		test.ok( !train.hasTag('cloud') );
+
+		test.done();
 	}
+
 }
