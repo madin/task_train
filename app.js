a2d58e9fae019bf868ffdb37e4d4ff5a5ea3a285
JuHyun Kwon <lanoir@gmail.com>
2016-03-23 08:45:26 +0900

    view engine 을 ejs 로 변경

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ db.init();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
